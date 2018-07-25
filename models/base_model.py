bd11455e1aaff99b754e7f5bfd512c6f22f9403a
kevanlucc <kevan.lucchini@gmail.com>
2018-07-25 02:32:48 +0000

    update kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -22,14 +22,15 @@ class BaseModel:
         '''
             Initialize public instance attributes.
         '''
-        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+            
+            if 'created_at' in kwargs: 
+                kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+            if 'updated_at' in kwargs:
+                kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
             for key, val in kwargs.items():
                 if "__class__" not in key:

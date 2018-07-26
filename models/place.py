b12d73480e194e9a2920b308b3be54528b0652cb
kevanlucc <kevan.lucchini@gmail.com>
2018-07-26 04:43:12 +0000

    fix formatting

--- models/place.py
+++ models/place.py
@@ -10,20 +10,18 @@ class Place(BaseModel):
         Define the class Place that inherits from BaseModel.
     '''
 
- __tablename__ = 'places'
+    __tablename__ = 'places'
 
-        city_id = Column(String(60),ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60),ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(FLOAT, nullable=True)
-        longitude = Column(FLOAT, nullable=True)
-
-
+    city_id = Column(String(60),ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60),ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
 
     city_id = ""
     user_id = ""

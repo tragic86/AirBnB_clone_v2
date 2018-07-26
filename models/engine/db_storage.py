19b1aa470654eccf1cd86c7eb9f8488d549e6c5e
kevanlucc <kevan.lucchini@gmail.com>
2018-07-26 04:44:46 +0000

    fix syntax

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,8 +12,9 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
-from sqlalchemy import scoped_session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
 
 class DBStorage:
     '''
@@ -26,18 +27,18 @@ class DBStorage:
         '''
         initializes the engine
         '''
+        user = getenv('HBNB_MYSQL_USER')
+        pw = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            user = os.getenv('HBNB_MYSQL_USER')
-            pw = os.getenv('HBNB_MYSQL_PWD')
-            host = os.getenv('HBNB_MYSQL_HOST')
-            db = os.getenv('HBNB_MYSQL_DB')
-            env = os.getenv('HBNB_ENV')),
-                                      pool_pre_ping=True)
+            user, pw, host, db), pool_pre_ping=True)
         Base.metadata.create_all(self.__engine)
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
-    
+
         session_factory = sessionmaker(self.__engine)
         self.__session = session_factory()
 
@@ -48,10 +49,9 @@ class DBStorage:
         """
         list_obj = {}
         if cls:
-            for obj in self.__session.query(cls).all()
-            key = str(obj.__class__.__name__) + "." + str(obj.id)
-            list_obj[key] = obj
-
+            for obj in self.__session.query(cls).all():
+                key = str(obj.__class__.__name__) + "." + str(obj.id)
+                list_obj[key] = obj
         else:
             others = [User, State, City, Amenity, Place, Review]
             for name in others:
@@ -63,7 +63,6 @@ class DBStorage:
     def new(self, obj):
         """add to session"""
         self.__session.add(obj)
-
 
     def save(self):
         """save session"""
@@ -77,6 +76,6 @@ class DBStorage:
     def reload(self):
         """reload session"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, 
+        session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         self.__session = scoped_session(session_factory)

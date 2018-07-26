cc5690265661fd094b7ec7ad2475336d92954851
kevanlucc <kevan.lucchini@gmail.com>
2018-07-26 02:54:17 +0000

    changed init back to previous form

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,26 +1,4 @@
 #!/usr/bin/python3
 '''
-    Package initializer
+    Pacakge initializer
 '''
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from os import getenv
-from models.amenity import Amenity
-from models.review import Review
-from models.engine.db_storage import DBStorage
-
-classes = {"User": User, "BaseModel": BaseModel,
-           "Place": Place, "State": State,
-           "City": City, "Amenity": Amenity,
-           "Review": Review}
-
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-    storage.reload()
-else:
-    storage = FileStorage()
-    storage.reload()

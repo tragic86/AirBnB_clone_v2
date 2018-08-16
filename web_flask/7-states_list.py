c13da9987b6814ee8e5b6609527e154dc07f301d
tragic86 <oneil5@aol.com>
2018-08-16 01:24:46 +0000

    state list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""importing flask and storage"""
+
+from flask import Flask
+from flask import render_template
+from models import State
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def s_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tearitdown():
+    storage.close()
